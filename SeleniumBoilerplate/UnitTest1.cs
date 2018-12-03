e74ec417c9560825d7bdeab32f2ab714041b1cb2
peace098beat <peace098beat.t@gmail.com>
2018-12-03 21:39:28 +0900

    First Commit

--- SeleniumBoilerplate/UnitTest1.cs
+++ SeleniumBoilerplate/UnitTest1.cs
@@ -1,14 +1,52 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+// Selenium関連パッケージ
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.UI;
+
+
 namespace SeleniumBoilerplate
 {
     [TestClass]
-    public class UnitTest1
+    public class BrowserTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestChromeDriver()
         {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+            }
+        }
+
+        [TestMethod]
+        public void TestFirefoxDriver()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+            }
+        }
+
+        [TestMethod]
+        public void TestEdgeDriver()
+        {
+            using (IWebDriver driver = new EdgeDriver())
+            {
+            }
+        }
+
+        [TestMethod]
+        public void TestPhantomJSDriver()
+        {
+            using (IWebDriver driver = new PhantomJSDriver())
+            {
+            }
         }
     }
 }
